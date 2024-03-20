bb1e5a6915427a1fa0ca2335eef217c1423fa874
Cristo Montoya <cristo.montoya1722@outlook.com>
2024-03-19 18:40:59 -0600

    finalizando el challenge

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
+
+
 function encriptar() {
     let texto = document.getElementById("texto").value;
     let text_desc = document.querySelector('#text_desc');
-  
+    let imagen = document.querySelector('#monito');
+
+ 
     let txtCifrado = texto
-      .replace(/e/gi, "enter")
-      .replace(/i/gi, "imes")
-      .replace(/a/gi, "ai")
-      .replace(/o/gi, "ober")
-      .replace(/u/gi, "ufat");
+      .replace(/e/, "enter")
+      .replace(/i/, "imes")
+      .replace(/a/, "ai")
+      .replace(/o/, "ober")
+      .replace(/u/, "ufat");
   
     if (texto.length != 0) {
       text_desc.value = txtCifrado;
+      imagen.classList.add('inactive');
+      text_desc.classList.remove('inactive');
       text_desc.classList.add('active');
+      
+    }else{
+        alert('Por favor escriba un mensaje');
     }
     
+  }
+  const desencriptar = () =>{
+    let texto = document.getElementById("texto").value;
+    let text_desc = document.querySelector('#text_desc');
+
+    let txtDes = texto
+    .replace(/enter/, "e")
+    .replace(/imes/, "i")
+    .replace(/ai/, "a")
+    .replace(/ober/, "o")
+    .replace(/ufat/, "u");
+
+    if (texto.length != 0) {
+        text_desc.value = txtDes
+    }else{
+        alert('Por favor escriba un mensaje');
+    }
+
+  }
+
+  const copiar = ()=>{
+    let text_desc = document.querySelector('#text_desc');
+    text_desc.select();
+    document.execCommand("copy")
+    alert('texto copiado en portapapeles');
   }
